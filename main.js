fa08d6cf6a341a931348aa325ed0b670a6c35a01
Yadira Guzman <yadiraguzman56@gmail.com>
2024-03-11 14:08:03 -0600

    proyecto

---
+++ main.js
@@ -0,0 +1,9 @@
+function ocultar(id) {
+    var div = document.getElementById(id);
+    if (div.style.display =='none' ){
+        div.style.display='flex'
+    }
+    else{
+        div.style.display ='none'
+    }
+}
